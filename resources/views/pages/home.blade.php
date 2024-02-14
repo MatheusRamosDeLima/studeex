44bf2b1e3816d0de2299ecabedf63b0d9b841a4f
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-02-14 19:45:44 -0300

    Adição de conteúdo e css para a página home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,22 @@
 @extends('layout')
 
+@section('css')
+<link rel="stylesheet" href="/assets/css/pages/home.css">
+@endsection
 @section('title', 'Studeex: Facilite a organização dos seus estudos')
 @section('content')
-<h1>Home</h1>
+<section id="choose-system">
+    <h1>Escolha o que deseja criar:</h1>
+    <div id="systems-create">
+        <a href="/criar/rotina">
+            <img src="/assets/imgs/icons/home/rotina.png" alt="Ícone de rotina">
+            <h2>Rotina de Estudos</h2>
+        </a>
+        <a href="/criar/ciclo">
+            <img src="/assets/imgs/icons/home/ciclo.png" alt="Ícone de ciclo">
+            <h2>Ciclo de Estudos</h2>
+        </a>
+    </div>
+</section>
+<section id="see-systems"></section>
 @endsection
